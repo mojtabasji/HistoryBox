9cfa36201f916124ed5a577e48cfe59d9f44a5ab
mojtaba <mojtabashojailine@yahoo.com>
2025-10-14 18:05:39 +0330

    Extra files were deleted

--- src/lib/auth0.ts
+++
@@ -1,9 +0,0 @@
-import { getSession } from '@auth0/nextjs-auth0';
-
-export async function requireAuth() {
-  const session = await getSession();
-  if (!session || !session.user) {
-    return { user: null } as const;
-  }
-  return { user: session.user } as const;
-}
